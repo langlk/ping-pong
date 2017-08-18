8b7c14b56403155ca00e1946c978360142b8889e
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-18 09:11:57 -0700

    Update pingPong to include Ping-Pong case.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@ function getError(input) {
 }
 
 function pingPong(number) {
-  if (number % 3 === 0) {
+  if (number % 15 === 0) {
+    return "Ping-Pong";
+  } else if (number % 3 === 0) {
     return "Ping";
   } else if (number % 5 === 0) {
     return "Pong";
