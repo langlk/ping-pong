2a2fca4107f2105d38e7cece2dcbf7f6441644be
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-18 09:06:50 -0700

    Add pingPong function with base case that returns number as-is.

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@ function getError(input) {
     return "Error: Invalid Input";
   } if (parseFloat(input) !== parseInt(input)) {
     return "Error: Invalid Input";
-  } 
+  }
+}
+
+function pingPong(number) {
+  return number;
 }
 
 $(document).ready(function () {
   $("#number").submit(function(event) {
     event.preventDefault();
     var range = $("input#range").val();
-    console.log(getError(range));
+    console.log(pingPong(range));
   });
 });
